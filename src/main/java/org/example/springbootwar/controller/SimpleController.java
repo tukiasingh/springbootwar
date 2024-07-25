b479775bf673fcb0cb6a3b14622f5c23e17576ee
tukiasingh <vijayprakashsingh0323@gmail.com>
2024-07-24 22:30:33 -0500

    changed to Rest Controller

--- src/main/java/org/example/springbootwar/controller/SimpleController.java
+++ src/main/java/org/example/springbootwar/controller/SimpleController.java
@@ -2,12 +2,13 @@ package org.example.springbootwar.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SimpleController {
 
     @GetMapping("/")
     public String greeting() {
-        return "home.html";
+        return "Welcome to Spring Boot War Deployment on External Tomcat Server!";
     }
 }
